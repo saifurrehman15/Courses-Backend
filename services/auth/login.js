814d2177f2924cb3140718414dd2b3d9d9ccbd23
saifurrehman15 <saifkhandada72@gmail.com>
2025-03-27 04:02:32 +0500

    login fixed

--- services/auth/login.js
+++ services/auth/login.js
@@ -1,14 +1,17 @@
 import { userModel } from "../../db-models/user-schema.js";
 import token from "../../helper/token-generate.js";
+import bcrypt from "bcryptjs";
 
 const loginService = async (value) => {
   const userExist = await userModel.findOne({ email: value.email });
 
   if (!userExist) return null;
+  let checkPassword = await bcrypt.compare(value.password, userExist.password);
+
+  if (!checkPassword) return null;
 
   let objWithoutPass = userExist.toObject();
   delete objWithoutPass.password;
-
   const { accessToken, refreshToken } = token(objWithoutPass);
 
   return { user: objWithoutPass, accessToken, refreshToken };
