ad6e195d9bb3e52222aa4c1b283313c685692b98
saifurrehman15 <saifkhandada72@gmail.com>
2025-04-04 01:40:54 +0500

    created by property added

--- src/app/courses/schema.js
+++ src/app/courses/schema.js
@@ -1,21 +1,23 @@
+import { required } from "joi";
 import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 const CourseSchema = new Schema(
-    {
-        title: { type: String, required: true },
-        description: { type: String, required: true },
-        // duration: { type: String, required: true },
-        // price: { type: Number, required: true },
-        // image: { type: String, required: true },
-        // level: { type: String, required: true },
-        // language: { type: String, required: true },
-        // students: { type: Number, required: true },
-    }
-    ,
-    {
-        timestamps: true,
-    }
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    createdBy: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    // duration: { type: String, required: true },
+    // price: { type: Number, required: true },
+    // image: { type: String, required: true },
+    // level: { type: String, required: true },
+    // language: { type: String, required: true },
+    // students: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
 );
-export const courseModel = mongoose.models.courses || mongoose.model("courses", CourseSchema);
+export const courseModel =
+  mongoose.models.courses || mongoose.model("courses", CourseSchema);
