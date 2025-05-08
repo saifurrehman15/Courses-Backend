5c1df8720ee46eb693e57898d4c8220531a59695
saifurrehman15 <saifkhandada72@gmail.com>
2025-05-08 22:45:47 +0500

    update

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors("*"));
-app.use(express.json());
-
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(
   session({
     name: "_ga",
@@ -44,7 +44,6 @@ cron.schedule("0 2 * * *", async () => {
   await client.flushDb();
   console.log("Redis cache cleared at 2 AM");
 });
-
 
 app.get("/", (req, res) => {
   res.send("Server is running on port " + port);
--- src/utils/configs/multer-config/index.js
+++ src/utils/configs/multer-config/index.js
@@ -1,6 +1,9 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
 
 export default upload;
